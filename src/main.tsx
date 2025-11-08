aee3696fa203a859c9d8384b3a02154ee40bc000
Prince Pal <rishabpal2000@gmail.com>
2025-11-08 19:02:09 +0530

    Added redux store, axios instance

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./main.css";
 import App from "./App.tsx";
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import store from "./store/reducers/store.ts";
+
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
 );
